c85ee98c6cf8fbc4ad5d7cf2f84f3a00542d2bf3
Ionut Ciochinaru <ciochinaru_ionut@yahoo.com>
2020-04-27 16:59:41 +0300

    api

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -3,7 +3,7 @@ import {OffersService} from './offers.service';
 import {OffersModel} from './offers.model';
 import {PlacesService} from '../places.service';
 import {Place} from '../place.model';
-import {IonItemSliding} from '@ionic/angular';
+import {IonItemSliding, LoadingController} from '@ionic/angular';
 import {Router} from '@angular/router';
 import {Subscription} from 'rxjs';
 
@@ -14,16 +14,29 @@ import {Subscription} from 'rxjs';
 })
 export class OffersPage implements OnInit, OnDestroy {
   offers: Place[];
+  isLoading = false;
   private placesSub: Subscription;
 
   constructor(private offersService: OffersService,
               private router: Router,
-              private placesService: PlacesService) { }
+              private placesService: PlacesService,
+              private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
   this.placesSub = this.placesService.places.subscribe(places => {
       this.offers = places;
     });
+  }
+
+  ionViewWillEnter() {
+    this.loadingCtrl.create({message: 'Fetching places...'}).then(
+        loadingEl => {
+          loadingEl.present();
+          this.placesService.fetchPlaces().subscribe(() => {
+            loadingEl.dismiss();
+          });
+        }
+    );
   }
 
   onEdit(offerId: string, slidingItem: IonItemSliding) {
--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -2,53 +2,70 @@ import {Injectable} from '@angular/core';
 import {Place} from './place.model';
 import {AuthService} from '../auth/auth.service';
 import {BehaviorSubject} from 'rxjs';
-import {delay, map, take, tap} from 'rxjs/operators';
+import {delay, map, switchMap, take, tap} from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
+
+interface PlaceData {
+    availableFrom: string;
+    availableTo: string;
+    description: string;
+    imageURL: string;
+    price: number;
+    title: string;
+    userId: string;
+}
 
 @Injectable({
     providedIn: 'root'
 })
 export class PlacesService {
-    private _places = new BehaviorSubject<Place[]>([
-        new Place(
-            'p1',
-            'house1',
-            'description is description',
-            'https://image.shutterstock.com/image-photo/luxurious-new-construction-home-bellevue-600w-555325381.jpg',
-            29.99,
-            new Date('2020-01-01'),
-            new Date('2020-12-31'),
-            'xyz'),
-        new Place('p2',
-            'house2',
-            'description is description2',
-            'https://image.shutterstock.com/image-photo/grey-small-house-porch-white-600w-120987520.jpg',
-            39.99,
-            new Date('2020-01-01'),
-            new Date('2020-12-31'),
-            'abc'),
-        new Place('p3',
-            'house3',
-            'description is description3',
-            'https://media.gettyimages.com/photos/dream-home-luxury-house-success-picture-id848549286?s=2048x2048',
-            120.52,
-            new Date('2020-01-01'),
-            new Date('2020-12-31'),
-            'abc'),
-    ]);
+    constructor(private authService: AuthService,
+                private http: HttpClient) {
+    }
+
+    private _places = new BehaviorSubject<Place[]>([]);
 
     get places() {
         return this._places.asObservable();
     }
 
+    fetchPlaces() {
+        return this.http
+            .get<{ [key: string]: PlaceData }>('https://booking-9e8e8.firebaseio.com/offered-places.json')
+            .pipe(map(resData => {
+                    const places = [];
+                    for (const key in resData) {
+                        if (resData.hasOwnProperty(key)) {
+                            places.push(new Place(
+                                key,
+                                resData[key].title,
+                                resData[key].description,
+                                resData[key].imageURL,
+                                resData[key].price,
+                                new Date(resData[key].availableFrom),
+                                new Date(resData[key].availableTo),
+                                resData[key].userId
+                                )
+                            );
+                        }
+                    }
+                     return places;
+                }),
+                tap(places => {
+                    this._places.next(places);
+                }));
+    }
+
     getPlace(id: string) {
-       return this.places.pipe(
-           take(1),
-           map(places => {
-               return {...places.find(p => p.id === id)};
-       }));
+        return this.places.pipe(
+            take(1),
+            map(places => {
+                return {...places.find(p => p.id === id)};
+            }));
     }
 
     addPlace(title: string, description: string, price: number, dateFrom: Date, dateTo: Date) {
+        let generatedId: string;
         const newPlace = new Place(
             Math.random().toString(),
             title,
@@ -59,10 +76,20 @@ export class PlacesService {
             dateTo,
             this.authService.userId
         );
-        return this.places.pipe(take(1), delay(1000), tap((x) => {
-            this._places.next(x.concat(newPlace));
-        }));
+        return this.http.post('https://booking-9e8e8.firebaseio.com/offered-places.json', {
+            ...newPlace, id: null
+        })
+            .pipe(switchMap(resData => {
+                    generatedId = resData.toString();
+                    return this.places;
+                }),
+                take(1),
+                tap(places => {
+                    newPlace.id = generatedId;
+                    this._places.next(places.concat(newPlace));
+                }));
     }
+
     editPlace(placeId: string, title: string, description: string) {
         return this.places.pipe(take(1), tap(places => {
             const updatedPlaceIndex = places.findIndex(pl => pl.id === placeId);
@@ -80,8 +107,5 @@ export class PlacesService {
             );
             this._places.next(updatedPlaces);
         }));
-    }
-
-    constructor( private authService: AuthService) {
     }
 }
